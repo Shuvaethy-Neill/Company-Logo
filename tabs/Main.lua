31848da82bf543f71a06d635d34b837f8242d101
Shuvaethy-Neill at codea-scm <Shuvaethy-Neill@codea-scm.aws.mapote.com>
2016-11-02 15:08:38 +0000

    init

---
+++ tabs/Main.lua
@@ -0,0 +1,26 @@
+-- Company logo
+
+-- Created by: Shuvaethy Neill
+-- Created on: Oct -2016
+-- Created for: ICS2O
+-- This program displays the company logo
+
+-- Use this function to perform your initial setup
+function setup()
+    supportedOrientations(LANDSCAPE_ANY)
+    displayMode(FULLSCREEN)
+    noFill()
+    noSmooth()
+    noStroke()
+    pushStyle()
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- Do your drawing here
+    sprite("Project:companyLogoImage", WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
+end
+
